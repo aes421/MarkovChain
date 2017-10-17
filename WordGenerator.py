b9b5c7716a47ed481a9ae0fc469b1db6f209006a
Ashley Stallings Rhodes <aesdev421@gmail.com>
2017-10-16 19:57:43 -0500

    begin of rewriting with Markov Order - not in working condition

--- WordGenerator.py
+++ WordGenerator.py
@@ -1,54 +1,73 @@
 import random
+import sys
 
 def learn():
-    startWords = []
+    filename = sys.argv[1]
+    if (len(sys.argv) == 3):
+        markovOrder = int(sys.argv[2])
+    else:
+        markovOrder = 1
+    startLetters = []
 
-    f = open("words_alpha.txt") # opens file for reading
-    mc = dict()# create dicionary for Markov chain
+    f = open(filename) # opens file for reading
+    mc = dict() # create dicionary for Markov chain
+
     for line in f: # iterate through the lines
         i = 0
-        words = list(line)
-        startWords.append(words[i]) # append first word of each line
-        while i <= len(words)-1:
-            if (i == len(words)-1):
-                if (mc.get(words[i])):
-                    mc[words[i]].append('')
+        letters = list(line)
+        if (len(letters) > markovOrder):
+            j = 0
+            startLetters.append(letters[i]) # append first word of each line
+
+            while i <= len(letters)-markovOrder:
+                # determine state based off markovOrder
+                currentState = ""
+                nextState = ""
+                j = 0
+                while j < markovOrder:
+                    currentState += letters[i + j]
+                    nextState += letters[i + 1 + j] # need to take into account if this is too far
+                    j += 1
+
+                if (i == len(letters)-markovOrder):
+                    if (mc.get(currentState)):
+                        mc[currentState].append('')
+                    else:
+                        mc[currentState] = ['']
+
                 else:
-                    mc[words[i]] = ['']
+                    if (mc.get(currentState)):
+                        # if it already exists append it
+                        mc[currentState].append(nextState)
+                    else:
+                        # if this is the first time we've seen it, create new array
+                        mc[currentState] = [nextState]
+                i += 1
 
-            else:
-                if (mc.get(words[i])):
-                    # if it already exists append it
-                    mc[words[i]].append(words[i+1])
-                else:
-                    # if this is the first time we've seen it, create new array
-                    mc[words[i]] = [words[i+1]]
-            i += 1
-
-    return startWords, mc
+    return startLetters, mc
 
 
-def generate(times, startWords, mc):
+def generate(times, startLetters, mc):
     i = 0
     while i < times:
         # choose a random start word
-        start = random.randrange(0, len(startWords))
-        sentence = startWords[start] # first word
-        words = mc[startWords[start]]
+        start = random.randrange(0, len(startLetters))
+        word = startLetters[start] # first word
+        letters = mc[startLetters[start]]
 
         # loop until termianted
         while(True):
             # randomly choose the next word
-            index = random.randrange(0, len(words))
-            sentence += words[index]
-            if (words[index] == ""):
+            index = random.randrange(0, len(letters))
+            word += letters[index]
+            if (letters[index] == ""):
                 break
             # get the new words lists
-            words = mc[words[index]]
+            letters = mc[letters[index]]
 
-        print(sentence)
+        print(word)
         i += 1
 
 if __name__ == '__main__':
-    startWords, mc = learn()
-    generate(10, startWords, mc)
+    startLetters, mc = learn()
+    generate(10, startLetters, mc)
